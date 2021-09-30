1f6f55fcfaa866906871bbffabff97b2f5affd47
Eunjae Lee <karis612@gmail.com>
2021-09-30 10:51:25 +0200

    add search-insights

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import aa from 'search-insights';
+
+aa('init', { appId: 'latency', apiKey: '6be0576ff61c053d5f9a3225e2a90f76' });
+aa('setUserToken', 'this-is-me');
+aa('clickedFilters', {
+  eventName: 'my filter event',
+  filters: ['brand:Apple'],
+  index: 'instant_search',
+});
 
 export default function App() {
   return (
